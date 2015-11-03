eeb963ae3aa14268b76f02b448a6d275252d76b5
Pozzi Gianmario <pozzigmario@gmail.com>
2015-11-03 10:11:41 +0100

    [MIG] Added migration state types

---
+++ orte/mca/mig/mig_types.h
@@ -0,0 +1,27 @@
+/*
+* Copyright (c) 2016 Politecnico di Milano, Inc.  All rights reserved.
+* $COPYRIGHT$
+* 
+* Additional copyrights may follow
+* 
+* $HEADER$
+*
+*/
+
+#ifndef MIG_TYPES_H
+#define	MIG_TYPES_H
+
+#include "orte_config.h"
+
+BEGIN_C_DECLS
+
+typedef enum{
+    MIG_NONE,
+    MIG_MOVING,
+    MIG_FINISHED,
+    MIG_ERROR
+}orte_mig_migration_state_t;
+        
+END_C_DECLS
+
+#endif	/* MIG_TYPES_H */
